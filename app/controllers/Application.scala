a74a62558b8d46d2e9751d8d316bbba9260778a1
DarkDaler <jesus.ruiz.91@gmail.com>
2014-09-18 12:54:56 +0200

    Entendiendo donde crear rutas e implementarlas

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,12 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-   Ok("Hello word")
+      Redirect(routes.Application.tasks)
   }
+
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 }
