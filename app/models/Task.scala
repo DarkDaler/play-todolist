7fb4279bf8308b3b9ef1314755aaf5b7489a213c
DarkDaler <jesus.ruiz.91@gmail.com>
2014-09-23 12:02:43 +0200

    Arreglada BD, y funcionando

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -2,7 +2,6 @@ package models
 
 import anorm._
 import anorm.SqlParser._
-
 import play.api.db._
 import play.api.Play.current
 
@@ -17,23 +16,23 @@ object Task {
       }
    }
 
-   def all(): List[Task] = DB.withConnection { implicit c =>
+   def all(): List[Task] = DB.withConnection {implicit c =>
       SQL("select * from task").as(task *)
    }
 
-   def create(label: String) {
-     DB.withConnection { implicit c =>
-       SQL("insert into task (label) values ({label})").on(
-         'label -> label
-       ).executeUpdate()
-     }
-   }  
+   def create(label: String){
+      DB.withConnection { implicit c =>
+         SQL("insert into task (label) values ({label})").on(
+            'label -> label
+         ).executeUpdate()
+      }
+   }
 
    def delete(id: Long) {
-     DB.withConnection { implicit c =>
-       SQL("delete from task where id = {id}").on(
-         'id -> id
-       ).executeUpdate()
-     }
+      DB.withConnection { implicit c =>
+         SQL("delete from task where id = {id}").on(
+            'id -> id
+         ).executeUpdate()
+      }
    }
 }
