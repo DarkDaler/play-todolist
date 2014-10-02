a079944ea379357aa75f56daf34ef4fb6a488ee1
DarkDaler <jesus.ruiz.91@gmail.com>
2014-10-02 12:36:54 +0200

    Creada nueva evolution2 para tabla SQL

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,13 @@
+# --- !Ups
+CREATE TABLE taskUser (
+   id varchar(255)PRIMARY KEY NOT NULL
+);
+
+ALTER TABLE task add idUser varchar(255);
+ALTER TABLE task add constraint id_user foreign key(idUser) references taskUser(id);
+
+# --- !Downs
+
+DROP TABLE IF EXISTS task;
+DROP SEQUENCE IF EXISTS task_id_seq;
+DROP TABLE IF EXISTS task-user;
