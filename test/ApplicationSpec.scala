e6f972152bd0f72f385537af1b76bcd4058f7a60
DarkDaler <jesus.ruiz.91@gmail.com>
2014-10-30 16:33:18 +0100

    Generados archivos de test de models y application

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,7 +1,10 @@
+package test
+
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
 
+import play.api.libs.json._
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -15,16 +18,5 @@ class ApplicationSpec extends Specification {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
-    }
-
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
-    }
   }
 }
