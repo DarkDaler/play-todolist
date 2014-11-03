8c57a4b4cf60df4726e6b74a704eef67c29698ce
DarkDaler <jesus.ruiz.91@gmail.com>
2014-11-03 23:57:12 +0100

    Models: Test task index vacia al insertarle en usuario registrado FEATURE2

--- test/ModelSpec.scala
+++ test/ModelSpec.scala
@@ -15,105 +15,121 @@ class ModelSpec extends Specification {
    def dateIs(date: java.util.Date, str: String) = new java.text.SimpleDateFormat("yyyy-MM-dd").format(date) == str  
    def strToDate(str: String) = new java.text.SimpleDateFormat("yyyy-MM-dd").parse(str)
 
-  "Models" should {
+  "Models FEATURE1" should {
 
-   //TESTS FEATURE 1
+        //TESTS FEATURE 1
 
-    "Test tasks vacias FEATURE 1" in {  
-        running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        "Test tasks vacias FEATURE 1" in {  
+            running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
 
-            // La BD está vacía  
-            val tasks = Task.all()
+                // La BD está vacía  
+                val tasks = Task.all()
 
-            tasks.isEmpty must equalTo(true)
+                tasks.isEmpty must equalTo(true)
+            }
         }
-    }
-    "Test busqueda task en vacia FEATURE 1" in {
-         running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
+        "Test busqueda task en vacia FEATURE 1" in {
+             running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
 
-            //La BD está vacia
-            val tasks = Task.getTasks(1)
+                //La BD está vacia
+                val tasks = Task.getTasks(1)
 
-            tasks.isEmpty must equalTo(true)
-         }
-    }
-    "Test crear una task y checkear que se ha creado FEATURE 1" in {
-         running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
+                tasks.isEmpty must equalTo(true)
+             }
+        }
+        "Test crear una task y checkear que se ha creado FEATURE 1" in {
+             running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
 
-            Task.create("prueba1", "anonimo")
+                Task.create("prueba1", "anonimo")
 
-            val tasks = Task.all()
+                val tasks = Task.all()
 
-            tasks.length must equalTo(1)
+                tasks.length must equalTo(1)
 
-            tasks.head.id must equalTo(1)
-            tasks.head.label must equalTo("prueba1")
+                tasks.head.id must equalTo(1)
+                tasks.head.label must equalTo("prueba1")
 
-         }
-    }
-    "Test checkear con getTasks(id) una tarea creada FEATURE 1" in {
-         running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
+             }
+        }
+        "Test checkear con getTasks(id) una tarea creada FEATURE 1" in {
+             running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
 
-            Task.create("prueba1", "anonimo")
+                Task.create("prueba1", "anonimo")
 
-            val tasks = Task.getTasks(1)
+                val tasks = Task.getTasks(1)
 
-            tasks.length must equalTo(1)
+                tasks.length must equalTo(1)
 
-            tasks.head.id must equalTo(1)
-            tasks.head.label must equalTo("prueba1")
+                tasks.head.id must equalTo(1)
+                tasks.head.label must equalTo("prueba1")
 
-         }
-    }
-    "Test crear varias tareas FEATURE1" in {
-         running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
+             }
+        }
+        "Test crear varias tareas FEATURE1" in {
+             running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
 
-            Task.create("prueba1", "anonimo")
-            Task.create("prueba2", "anonimo")
-            Task.create("prueba3", "anonimo")
-            Task.create("prueba4", "anonimo")
+                Task.create("prueba1", "anonimo")
+                Task.create("prueba2", "anonimo")
+                Task.create("prueba3", "anonimo")
+                Task.create("prueba4", "anonimo")
 
-            val tasks = Task.all()
+                val tasks = Task.all()
 
-            tasks.length must equalTo(4)
+                tasks.length must equalTo(4)
 
-            tasks.last.label must equalTo("prueba4")
+                tasks.last.label must equalTo("prueba4")
 
-         }
-    }
-    "Test crear y eliminar una tarea FEATURE1" in {
-         running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
+             }
+        }
+        "Test crear y eliminar una tarea FEATURE1" in {
+             running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
 
-            Task.create("prueba1", "anonimo")
+                Task.create("prueba1", "anonimo")
 
-            val tasks = Task.all()
+                val tasks = Task.all()
 
-            tasks.length must equalTo(1)
+                tasks.length must equalTo(1)
 
-            Task.delete(1)
+                Task.delete(1)
 
-            val tasksNew = Task.all()
+                val tasksNew = Task.all()
 
-            tasksNew.length must equalTo(0)
-         }
-    }
-    "Test eliminar una tarea inexistente FEATURE1" in {
-         running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
+                tasksNew.length must equalTo(0)
+             }
+        }
+        "Test eliminar una tarea inexistente FEATURE1" in {
+             running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
 
-            Task.create("prueba1", "anonimo")
+                Task.create("prueba1", "anonimo")
 
-            val tasks = Task.all()
+                val tasks = Task.all()
 
-            tasks.length must equalTo(1)
+                tasks.length must equalTo(1)
 
-            Task.delete(2)
+                Task.delete(2)
 
-            val tasksNew = Task.all()
+                val tasksNew = Task.all()
 
-            tasksNew.length must equalTo(1)
+                tasksNew.length must equalTo(1)
 
-         }
-    }
+             }
+        }
 
    } 
+
+   "Models FEATURE2" should {
+
+        "Test task index vacia al insertarle en usuario registrado" in {
+            running(FakeApplication(additionalConfiguration = inMemoryDatabase())){
+
+                Task.create("prueba1", "admin")
+
+                val tasks = Task.all()
+
+                tasks.length must equalTo(0)
+            }
+
+        }
+
+   }
 }
