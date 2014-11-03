0f2028dfd667d642e11da2678d39ac2ce2058ded
DarkDaler <jesus.ruiz.91@gmail.com>
2014-11-04 00:54:56 +0100

    Application: Prueba tasks vacias de user admin y anonimo FEATURE2

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -16,139 +16,166 @@ import play.api.test.Helpers._
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
-  "Application" should {
+  "Application FEATURE1" should {
 
-   //FEATURE 1
+     //FEATURE 1
 
-   "Envio 404 de peticion erronea" in {
-      running(FakeApplication()){
-        route(FakeRequest(GET, "/erronea")) must beNone
+     "Envio 404 de peticion erronea" in {
+        running(FakeApplication()){
+          route(FakeRequest(GET, "/erronea")) must beNone
+        }
+     }
+
+     "Prueba pagina index" in {
+        running(FakeApplication()){
+          val Some(home) = route(FakeRequest(GET, "/"))
+
+          status(home) must equalTo(OK)  
+          contentType(home) must beSome.which(_ == "text/html")   
+        }
+     }
+
+     "Prueba pagina task vacia FEATURE 1" in {
+        running(FakeApplication()){
+          val Some(tasks) = route(FakeRequest(GET, "/tasks"))
+
+          status(tasks) must equalTo(OK)
+          val json = contentAsJson(tasks)
+          var jsonString = Json.stringify(json)
+
+          jsonString must equalTo("[]")
+        }
+     }
+
+     "Prueba introducir 1 tarea FEATURE 1" in {
+        running(FakeApplication()){
+
+          val Some(tasks) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba"))
+
+          status(tasks) must equalTo(CREATED)
+          val json =contentAsJson(tasks)
+          var jsonString = Json.stringify(json)
+
+          jsonString must equalTo("[{\"id\":1,\"label\":\"prueba\"}]")
+        }
+     } 
+
+     "Prueba introducir varias tareas FEATURE 1" in {
+        running(FakeApplication()){
+
+          val Some(post1) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba1"))
+          status(post1) must equalTo(CREATED)
+
+          val Some(post2) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba2"))
+          status(post2) must equalTo(CREATED)
+
+          val Some(post3) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba3"))
+          status(post3) must equalTo(CREATED)
+
+          val Some(post4) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba4"))
+          status(post4) must equalTo(CREATED)
+          
+          val Some(tasks) = route(FakeRequest(GET, "/tasks"))
+
+          status(tasks) must equalTo(OK)
+          val json =contentAsJson(tasks)
+          var jsonString = Json.stringify(json)
+
+          jsonString must equalTo("[{\"id\":1,\"label\":\"prueba1\"},{\"id\":2,\"label\":\"prueba2\"},{\"id\":3,\"label\":\"prueba3\"},{\"id\":4,\"label\":\"prueba4\"}]")
+        }
       }
-   }
 
-   "Prueba pagina index" in {
-      running(FakeApplication()){
-        val Some(home) = route(FakeRequest(GET, "/"))
+      "Prueba getTask(id) de tarea existente FEATURE1" in {
+        running(FakeApplication()){
 
-        status(home) must equalTo(OK)  
-        contentType(home) must beSome.which(_ == "text/html")   
+          val Some(post1) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba1"))
+          status(post1) must equalTo(CREATED)
+
+          val Some(getTask) = route(FakeRequest(GET, "/tasks/1"))
+
+          status(getTask) must equalTo(OK)
+          val json =contentAsJson(getTask)
+          var jsonString = Json.stringify(json)
+
+          jsonString must equalTo("[{\"id\":1,\"label\":\"prueba1\"}]")
+        }
       }
-   }
 
-   "Prueba pagina task vacia FEATURE 1" in {
-      running(FakeApplication()){
-        val Some(tasks) = route(FakeRequest(GET, "/tasks"))
+      "Prueba getTask(id) de tarea no existente FEATURE1" in {
+        running(FakeApplication()){
 
-        status(tasks) must equalTo(OK)
-        val json = contentAsJson(tasks)
-        var jsonString = Json.stringify(json)
+          val Some(getTask) = route(FakeRequest(GET, "/tasks/1"))
 
-        jsonString must equalTo("[]")
+          status(getTask) must equalTo(404)
+          contentAsString(getTask) must contain("No existe la tarea")
+        }
       }
-   }
 
-   "Prueba introducir 1 tarea FEATURE 1" in {
-      running(FakeApplication()){
+      "Prueba eliminar una tarea FEATURE1" in {
+        running(FakeApplication()){
 
-        val Some(tasks) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba"))
+          val Some(post1) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba1"))
+          status(post1) must equalTo(CREATED)
 
-        status(tasks) must equalTo(CREATED)
-        val json =contentAsJson(tasks)
-        var jsonString = Json.stringify(json)
+          val Some(post2) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba2"))
+          status(post2) must equalTo(CREATED)
 
-        jsonString must equalTo("[{\"id\":1,\"label\":\"prueba\"}]")
+          val Some(post3) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba3"))
+          status(post3) must equalTo(CREATED)
+
+          val Some(deleteTask) = route(FakeRequest(DELETE, "/tasks/1"))
+          redirectLocation(deleteTask) must beSome.which(_ == "/tasks")
+
+          val Some(tasks) = route(FakeRequest(GET, "/tasks"))
+          val json =contentAsJson(tasks)
+          var jsonString = Json.stringify(json)
+          jsonString must equalTo("[{\"id\":2,\"label\":\"prueba2\"},{\"id\":3,\"label\":\"prueba3\"}]")
+        }
       }
-   } 
 
-   "Prueba introducir varias tareas FEATURE 1" in {
-      running(FakeApplication()){
+      "Prueba a eliminar una tarea inexistente FEATURE1" in {
+        running(FakeApplication()){
 
-        val Some(post1) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba1"))
-        status(post1) must equalTo(CREATED)
+          val Some(post1) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba1"))
+          status(post1) must equalTo(CREATED)
 
-        val Some(post2) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba2"))
-        status(post2) must equalTo(CREATED)
+          val Some(deleteTask) = route(FakeRequest(DELETE, "/tasks/2"))
+          status(deleteTask) must equalTo(404)
+          contentAsString(deleteTask) must contain("No existe la tarea")
 
-        val Some(post3) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba3"))
-        status(post3) must equalTo(CREATED)
+          val Some(tasks) = route(FakeRequest(GET, "/tasks"))
+          val json =contentAsJson(tasks)
+          var jsonString = Json.stringify(json)
+          jsonString must equalTo("[{\"id\":1,\"label\":\"prueba1\"}]")
+        }
+      }
 
-        val Some(post4) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba4"))
-        status(post4) must equalTo(CREATED)
+  }
+
+  "Application FEATURE2" should {
+      "Prueba tasks vacias de user admin y anonimo FEATURE 2" in{
+        running(FakeApplication()){
+
+          val Some(tasksAnonimo) = route(FakeRequest(GET, "/users/anonimo/tasks"))
+          status(tasksAnonimo) must equalTo(OK)
+
+          val json = contentAsJson(tasksAnonimo)
+          var jsonString = Json.stringify(json)
+
+          jsonString must equalTo("[]")
+
+          val Some(tasksAdmin) = route(FakeRequest(GET, "/users/admin/tasks"))
+          status(tasksAdmin) must equalTo(OK)
+
+          val json2 = contentAsJson(tasksAdmin)
+          var jsonString2 = Json.stringify(json2)
+
+          jsonString2 must equalTo("[]")
+
+        }
+      }
+
         
-        val Some(tasks) = route(FakeRequest(GET, "/tasks"))
-
-        status(tasks) must equalTo(OK)
-        val json =contentAsJson(tasks)
-        var jsonString = Json.stringify(json)
-
-        jsonString must equalTo("[{\"id\":1,\"label\":\"prueba1\"},{\"id\":2,\"label\":\"prueba2\"},{\"id\":3,\"label\":\"prueba3\"},{\"id\":4,\"label\":\"prueba4\"}]")
-      }
-    }
-
-    "Prueba getTask(id) de tarea existente FEATURE1" in {
-      running(FakeApplication()){
-
-        val Some(post1) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba1"))
-        status(post1) must equalTo(CREATED)
-
-        val Some(getTask) = route(FakeRequest(GET, "/tasks/1"))
-
-        status(getTask) must equalTo(OK)
-        val json =contentAsJson(getTask)
-        var jsonString = Json.stringify(json)
-
-        jsonString must equalTo("[{\"id\":1,\"label\":\"prueba1\"}]")
-      }
-    }
-
-    "Prueba getTask(id) de tarea no existente FEATURE1" in {
-      running(FakeApplication()){
-
-        val Some(getTask) = route(FakeRequest(GET, "/tasks/1"))
-
-        status(getTask) must equalTo(404)
-        contentAsString(getTask) must contain("No existe la tarea")
-      }
-    }
-
-    "Prueba eliminar una tarea FEATURE1" in {
-      running(FakeApplication()){
-
-        val Some(post1) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba1"))
-        status(post1) must equalTo(CREATED)
-
-        val Some(post2) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba2"))
-        status(post2) must equalTo(CREATED)
-
-        val Some(post3) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba3"))
-        status(post3) must equalTo(CREATED)
-
-        val Some(deleteTask) = route(FakeRequest(DELETE, "/tasks/1"))
-        redirectLocation(deleteTask) must beSome.which(_ == "/tasks")
-
-        val Some(tasks) = route(FakeRequest(GET, "/tasks"))
-        val json =contentAsJson(tasks)
-        var jsonString = Json.stringify(json)
-        jsonString must equalTo("[{\"id\":2,\"label\":\"prueba2\"},{\"id\":3,\"label\":\"prueba3\"}]")
-      }
-    }
-
-    "Prueba a eliminar una tarea inexistente FEATURE1" in {
-      running(FakeApplication()){
-
-        val Some(post1) = route(FakeRequest(POST, "/tasks").withFormUrlEncodedBody("label" -> "prueba1"))
-        status(post1) must equalTo(CREATED)
-
-        val Some(deleteTask) = route(FakeRequest(DELETE, "/tasks/2"))
-        status(deleteTask) must equalTo(404)
-        contentAsString(deleteTask) must contain("No existe la tarea")
-
-        val Some(tasks) = route(FakeRequest(GET, "/tasks"))
-        val json =contentAsJson(tasks)
-        var jsonString = Json.stringify(json)
-        jsonString must equalTo("[{\"id\":1,\"label\":\"prueba1\"}]")
-      }
-    }
 
   }
 }
